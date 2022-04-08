9ccb2f6bf4e1b6305c4e592ad0f897c761f8bfff
CodyMarkix <hplr63@gmail.com>
2022-04-08 14:50:23 +0200

    begun writing the online updater

---
+++ src/updater/updater.py
@@ -0,0 +1,11 @@
+# Importing libraries
+import requests
+
+# DO NOT CHANGE THIS VARIABLE, OTHERWISE, THE UPDATER BREAKS
+CURRENTVER = "0.9"
+
+def check():
+    requests.get()
+
+def update():
+    print("update")
