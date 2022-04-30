9d537bfba8594e59a9c5fd799b7d7066e19cb002
CodyMarkix <hplr63@gmail.com>
2022-04-30 17:37:44 +0200

    fixed bug with update checker

--- src/updatemgr/checker.py
+++ src/updatemgr/checker.py
@@ -1,16 +1,22 @@
 import os
 import requests
-import codecs
 import json
 
 def checkForUpdates():
     with open(os.environ['HOME'] +"/.local/share/pyz/version.json", "r+") as versionjson:
-        remotejson = requests.get("https://raw.githubusercontent.com/CodyMarkix/PyZ-shell/master/version.json")
+        with open('/tmp/remotejson.json', 'x') as remotejsoncr:
+            remotejsoncr.close()
+
+        with open('/tmp/remotejson.json', 'wb') as remotejson:
+            remotejsonrq = requests.get("https://raw.githubusercontent.com/CodyMarkix/PyZ-shell/master/version.json")
+            remotejson.write(remotejsonrq.content)
+        
         localjson = json.loads(versionjson.read())
+        remotejson = json.loads(open('/tmp/remotejson.json', 'r').read())
 
-        print(json.dumps(codecs.decode(remotejson.content)), localjson)
-
-        # if codecs.decode(remotejson.content) == versionjson.readlines():
-        #     return
-        # else:
-        #     print(f'You have new updates available! Check the GitHub repo/your package manager.\n')
+        if localjson == remotejson:
+            os.remove('/tmp/remotejson.json')
+            return
+        else:
+            print(f'You have new updates available! Check the GitHub repo/your package manager.\n')
+            os.remove('/tmp/remotejson.json')
