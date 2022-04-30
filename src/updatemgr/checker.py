bf8e7542595e2427e1f06b6c4d73ba8744d6f11a
CodyMarkix <hplr63@gmail.com>
2022-04-30 17:27:03 +0200

    fixing update checking pt. 1

--- src/updatemgr/checker.py
+++ src/updatemgr/checker.py
@@ -1,13 +1,16 @@
 import os
 import requests
+import codecs
+import json
 
 def checkForUpdates():
-    with open(os.environ['HOME'] +"/.local/share/pyz/version.json", "w+") as versionjson:
+    with open(os.environ['HOME'] +"/.local/share/pyz/version.json", "r+") as versionjson:
         remotejson = requests.get("https://raw.githubusercontent.com/CodyMarkix/PyZ-shell/master/version.json")
-        print(remotejson.content)
-        print(versionjson.read())
+        localjson = json.loads(versionjson.read())
 
-        if remotejson.content == versionjson.readlines():
-            return
-        else:
-            print(f'You have new updates available! Check the GitHub repo/your package manager.\n')
+        print(json.dumps(codecs.decode(remotejson.content)), localjson)
+
+        # if codecs.decode(remotejson.content) == versionjson.readlines():
+        #     return
+        # else:
+        #     print(f'You have new updates available! Check the GitHub repo/your package manager.\n')
--- src/updatemgr/verJSONcreate.py
+++ src/updatemgr/verJSONcreate.py
@@ -9,5 +9,5 @@ def createVersionJSON():
     # jsoncreating = open(os.environ['HOME']+ "/.local/share/pyz/version.json", "x")
     # jsoncreating.close()
 
-    with open(os.environ['HOME'] +"/version.json", "w+") as versionjson:
+    with open(os.environ['HOME'] +"/.local/share/pyz/version.json", "w+") as versionjson:
         json.dump(samplejsonnp, versionjson)
