42cc49029f2b39fa7e9fdac89ca4a8ebeeb5aded
CodyMarkix <hplr63@gmail.com>
2022-04-30 17:12:58 +0200

    ok for real this time

--- src/updatemgr/checker.py
+++ src/updatemgr/checker.py
@@ -2,9 +2,12 @@ import os
 import requests
 
 def checkForUpdates():
-    with open(os.environ['HOME'] +"/version.json", "w+") as versionjson:
+    with open(os.environ['HOME'] +"/.local/share/pyz/version.json", "w+") as versionjson:
         remotejson = requests.get("https://raw.githubusercontent.com/CodyMarkix/PyZ-shell/master/version.json")
-        if remotejson.content == versionjson.read():
+        print(remotejson.content)
+        print(versionjson.read())
+
+        if remotejson.content == versionjson.readlines():
             return
         else:
             print(f'You have new updates available! Check the GitHub repo/your package manager.\n')
